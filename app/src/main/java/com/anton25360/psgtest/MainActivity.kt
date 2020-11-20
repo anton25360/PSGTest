589ce5633cf5959b3055264375622735df673466
Anton Webb <anton25360@gmail.com>
2020-11-20 14:41:39 +0000

    gets relevant video data from api

--- app/src/main/java/com/anton25360/psgtest/MainActivity.kt
+++ app/src/main/java/com/anton25360/psgtest/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.anton25360.psgtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
 import org.json.JSONArray
+import org.json.JSONObject
 import java.io.IOException
 
+
+val TAG = "xxx"
+
+@Suppress("NAME_SHADOWING", "NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,28 +24,34 @@ class MainActivity : AppCompatActivity() {
     private fun fetchData() {
 
         Toast.makeText(this, "getting data...", Toast.LENGTH_SHORT).show()
-        val url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDY9AyHAa43UnviLtl0GaynGUmEyAvlr5k&playlistId=UU_A--fhX5gea0i4UtpD99Gg&part=snippet&maxResults=50"
+        val url = "https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDY9AyHAa43UnviLtl0GaynGUmEyAvlr5k&playlistId=UU_A--fhX5gea0i4UtpD99Gg&part=snippet&maxResults=5"
         val request = Request.Builder().url(url).build()
 
         OkHttpClient().newCall(request).enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
                 val data: String? = response.body?.string() //response as a string
-                val jsonArray = JSONArray(data) //convert to json array
+                val dataFromApi = JSONObject(data)["items"].toString() //convert to json object
 
-                Log.d("TAG", "onResponse: $data")
+                val dataArray = emptyArray<Any>() //empty array to put data from loop in
 
-                //loop through original json array:
-//                for (i in 0 until jsonArray.length()) {
-//                    val item = jsonArray.getJSONObject(i)
-//
-//                    if (item.get("available") == "yes") { //if item is available
-//                        availableItems.add(item.get("item_name")) //add it to the array
-//                    }
-//                }
+                //loop through json array and print all titles:
+                for (i in 0 until JSONArray(dataFromApi).length()) {
+                    val videoData = JSONArray(dataFromApi)[i].toString() //gets the data of a single video
+                    val mSnippet = JSONObject(videoData)["snippet"].toString()
+                    val mThumbnails = JSONObject(mSnippet)["thumbnails"].toString()
+                    val mDefault = JSONObject(mThumbnails)["default"].toString()
+                    val mResourceId = JSONObject(mSnippet)["resourceId"].toString()
+
+                    val title = JSONObject(mSnippet)["title"]
+                    val url = JSONObject(mDefault)["url"].toString()
+                    val videoId = JSONObject(mResourceId)["videoId"].toString()
+
+                    Log.d(TAG, "onResponse: $title + $url + $videoId")
+                }
             }
 
             override fun onFailure(call: Call, e: IOException) {
-                println(e)
+                Log.d(TAG, "onFailure: $e")
             }
         })
 
